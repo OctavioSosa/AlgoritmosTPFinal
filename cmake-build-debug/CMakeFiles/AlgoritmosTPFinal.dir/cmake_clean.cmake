cadb63506c9c58d4afce65aefb9928065a48cb1d
OctavioSosa <ocktavio123@gmail.com>
2021-06-27 20:09:55 -0300

    Makefile y archivos bash

---
+++ cmake-build-debug/CMakeFiles/AlgoritmosTPFinal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AlgoritmosTPFinal"
+  "AlgoritmosTPFinal.pdb"
+  "CMakeFiles/AlgoritmosTPFinal.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgoritmosTPFinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
